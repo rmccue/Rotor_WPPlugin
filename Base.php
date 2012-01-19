761cedeba5147e5ef01361fc8f59fd03931dfe97
Ryan McCue <me@ryanmccue.info>
2012-01-19 15:33:55 +1000

    Add support for custom prefixes, including no prefix

--- Base.php
+++ Base.php
@@ -13,10 +13,18 @@ abstract class Rotor_WPPlugin_Base {
 	 *
 	 * @see Rotor_WP_Plugin::__construct
 	 * @see Rotor_WP_Plugin::register_hooks
-	 * @param boolean $enable_prefixes Whether to enable prefixed methods (i.e. `action_init` or `filter_the_title`)
+	 * @param boolean|array $prefixes True for default (`action_`/`filter_`), array with keys "action" & "filter" or false
 	 * @param string|object $parent Object to register from
 	 */
-	protected static function _register_hooks($enable_prefixes, $parent) {
+	protected static function _register_hooks($prefixes, $parent) {
+		$enable_prefixes = true;
+		if ($prefixes === false) {
+			$enable_prefixes = false;
+		}
+		elseif ($prefixes === true) {
+			$prefixes = array('filter' => 'filter_', 'action' => 'action_');
+		}
+
 		$self = new ReflectionClass($parent);
 		foreach ($self->getMethods(ReflectionMethod::IS_PUBLIC) as $method) {
 			$params = $method->getNumberOfParameters();
@@ -27,15 +35,27 @@ abstract class Rotor_WPPlugin_Base {
 
 			$hooks = array('filter' => array(), 'action' => array());
 
-			if ($enable_prefixes === true && strpos($method->name, 'filter_') === 0) {
-				$hook = substr($method->name, 7);
-				$hooks['action'][$hook] = 10;
+			if ($enable_prefixes === true) {
+				// If either prefix is blank, always hook
+				if ($prefixes['filter'] === '' || $prefixes['action'] === '') {
+					$hooks['filter'][$method->name] = 10;
+				}
+
+				// Method starts with filter prefix
+				elseif ($enable_prefixes === true && strpos($method->name, $prefixes['filter']) === 0) {
+					$hook = substr($method->name, strlen($prefixes['filter']));
+					$hooks['action'][$hook] = 10;
+				}
+
+				// Method starts with action prefix
+				elseif ($enable_prefixes === true && strpos($method->name, $prefixes['action']) === 0) {
+					$hook = substr($method->name, strlen($prefixes['action']));
+					$hooks['action'][$hook] = 10;
+				}
 			}
-			elseif ($enable_prefixes === true && strpos($method->name, 'action_') === 0) {
-				$hook = substr($method->name, 7);
-				$hooks['action'][$hook] = 10;
-			}
-			else {
+
+			// If we haven't hooked anything yet, check phpdoc
+			if (empty($hooks['filter']) && empty($hooks['action'])) {
 				if (empty($doc) || (strpos($doc, '@wp-filter') === false && strpos($doc, '@wp-action') === false)) {
 					continue;
 				}
