ab24a23c60846016d1af040bef864d87ae8a7cdc
Ryan McCue <me@ryanmccue.info>
2012-01-19 17:24:54 +1000

    Check that eAccelerator is sane.
    If there are any other opcode caches that strip this, let me know.
    
    We could simply run the test every time, but that would introduce overhead for every system instead of those running eAccelerator.

--- Base.php
+++ Base.php
@@ -15,6 +15,12 @@ class Rotor_WPPlugin_Base {
 	 * @param string|object $parent Object to register from
 	 */
 	public static function _register_hooks($prefixes, $parent) {
+		$is_sane = self::check_eaccelerator_saneness();
+		if (!$is_sane) {
+			// This will be replaced with something better soon
+			throw new Exception('eAccelerator is configured to strip doc comments, cannot continue');
+		}
+
 		$enable_prefixes = true;
 		if ($prefixes === false) {
 			$enable_prefixes = false;
@@ -81,5 +87,22 @@ class Rotor_WPPlugin_Base {
 				call_user_func(array($parent, 'add_action'), $hook, $method->name, $priority, $params, $parent);
 			}
 		}
+	}
+
+	/**
+	 * Check if eAccelerator is loaded, and if so, is sane
+	 *
+	 * @internal If you can find me, it's sane.
+	 * @return boolean True if doc comments are fine, false otherwise
+	 */
+	private static function check_eaccelerator_saneness() {
+		if (!extension_loaded('eaccelerator') && !extension_loaded('eAccelerator')) {
+			return true;
+		}
+
+		$method = new ReflectionMethod('Rotor_WPPlugin_Base', 'check_eaccelerator_saneness');
+		$comment = $method->getDocComment();
+
+		return (strpos($comment, "If you can find me, it's sane.") !== false);
 	}
 }
